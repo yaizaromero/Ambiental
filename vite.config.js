528f4d2b1914ce9d824d5edfd7af582963e5f1c9
MarioRodriguezuc3m <100472194@alumnos.uc3m.es>
2026-01-15 14:21:52 +0100

    otro mini cambio

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   worker: {
-    format: 'es', // Obligatorio para que funcionen los workers como módulos
-  },
-  build: {
-    target: 'esnext' // <--- AÑADE ESTO: Evita errores al construir la versión final
+    format: 'es',
   },
   optimizeDeps: {
     exclude: ['@xenova/transformers', 'onnxruntime-web'],
